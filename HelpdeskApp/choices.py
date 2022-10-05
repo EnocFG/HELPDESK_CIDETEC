ea07c5e6544d536ce26d6e7e8c6e62a3ec556d4b
Jennifer Moctezuma <jennifermoctezumal@gmail.com>
2022-10-05 17:10:51 -0500

    añadi tablas

---
+++ HelpdeskApp/choices.py
@@ -0,0 +1,25 @@
+status= (
+    ('1', 'Creado')
+    ('2', 'Atendido')
+    ('3', 'Asignado')
+    ('4','En proceso')
+    ('5','Espera')
+    ('6','Resuelto')
+    ('7','Validado')
+    ('8','Cancelado')
+    ('9','Reasignado')
+)
+
+prioridad=(
+    ('1','Urgente')
+    ('2', 'Alta')
+    ('3', 'Baja')
+    ('4', 'Media')
+)
+
+rol=(
+    ('SA', 'Super Administrador')
+    ('GP', 'Gestor proyecto')
+    ('ES', 'Especialista')
+    ('UF', 'Operador')
+)
--- HelpdeskApp/models.py
+++ HelpdeskApp/models.py
@@ -1,71 +1,96 @@
 from django.db import models
 
-# Create your models here.
+
+class status_entidad (models.Model):
+
+    tipo_estatus = models.CharField(max_length=10)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+
 class proyecto (models.Model):
 
-    nombre_proyecto=models.CharField(max_length=40)
-    codigo_proyecto=models.CharField(max_length=5)
-    fecha_inicio=models.DateTimeField(verbose_name='Fecha inicio')
-    fecha_culminacion = models.DateTimeField(verbose_name='Fecha culminacion')
-    descripcion=models.BigIntegerField()
-
-class rol(models.Model):
-
-    tipo_rol = models.CharField(max_length=40)
-
-class especialidad(models.Model):
-
-    tipo_especialidad = models.CharField(max_length=40)
-
-
-
-class status(models.Model):
-
-    tipo_status = models.CharField(max_length=40)
-
-
-class prioridad(models.Model):
-
-    tipo_prioridad = models.CharField(max_length=40)
-
+    nombre_proyecto = models.CharField(max_length=10)
+    codigo_proyecto = models.CharField(max_length=5)
+    fecha_inicial = models.DateTimeField(verbose_name='Fecha incial')
+    fecha_final = models.DateTimeField(verbose_name='Fecha final')
+    descripcion  = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+    status_entidad = models.ForeignKey(status_entidad , on_delete=models.CASCADE)
 
 class area (models.Model):
 
-    nombre_area = models.CharField(max_length=40)
+    nombre_area = models.CharField(max_length=10)
     codigo_area = models.CharField(max_length=5)
-    id_proyecto = models.ForeignKey(proyecto, on_delete=models.CASCADE)
+    fecha_inicial = models.DateTimeField(verbose_name='Fecha incial')
+    fecha_final = models.DateTimeField(verbose_name='Fecha final')
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+    proyecto_area = models.ForeignKey(proyecto, on_delete=models.CASCADE)
+    status_entidad = models.ForeignKey(status_entidad , on_delete=models.CASCADE)
 
+class rol (models.Model):
 
+    tipo_rol= models.CharField(max_length=30)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+class especalidad (models.Model):
+
+    tipo_especialidad  = models.CharField(max_length=10)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+class area_usuario (models.Model):
+
+    usuario= models.ForeignKey(usuario, on_delete=models.CASCADE)
+    proyecto= models.ForeignKey(proyecto, on_delete=models.CASCADE)
+    area= models.ForeignKey(area, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
 class usuario (models.Model):
 
-    nombre_usuario = models.CharField(max_length=40)
-    apellidos_usuario = models.CharField(max_length=40)
-    email_usuario = models.EmailField()
-    password_usuario = models.CharField(max_length=40)
-    rol_usuario = models.ForeignKey(rol, on_delete=models.CASCADE)
-    especialidad = models.ForeignKey(especialidad, on_delete=models.CASCADE)
-    proyecto = models.ForeignKey(proyecto, on_delete=models.CASCADE)
-    area = models.ForeignKey(area, on_delete=models.CASCADE)
+    codigo_usuario = models.CharField(max_length=5)
+    nombre_usuario = models.CharField(max_length=30, verbose_name='Nombre del usuario')
+    apellidos_usuario= models.CharField(max_length=30, verbose_name='Appellios del usuario')
+    email_usuario= models.EmailField(verbose_name='Nombre del usuario')
+    password = models.CharField(max_length=8)
+    usuario_rol= models.ForeignKey(rol, on_delete=models.CASCADE)
+    usuario_proyecto= models.ForeignKey(proyecto, on_delete=models.CASCADE)
+    usuario_area= models.ForeignKey(area_usuario, on_delete=models.CASCADE)
+    usuario_especialista= models.ForeignKey(especialista, on_delete=models.CASCADE)
+    status_entidad = models.ForeignKey(status_entidad, on_delete=models.CASCADE)
+class especialista (models.Model):
 
-class especialista(models.Model):
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+    status_entidad = models.ForeignKey(status_entidad, on_delete=models.CASCADE)
+    especialista_especialidad = models.ForeignKey(especalidad, on_delete=models.CASCADE)
+    especialista_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
 
-    tipo_especialidad = models.ForeignKey(especialidad, on_delete=models.CASCADE)
-    id_usuario= models.ForeignKey(usuario, on_delete=models.CASCADE)
+class prioridad (models.Model):
+    tipo_prioridad = models.CharField(max_length=30)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+class status_ticket (models.Model):
 
-
+    tipo_estatus = models.CharField(max_length=10)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
 class ticket (models.Model):
 
-    folio= models.CharField(max_length=40)
-    titulo= models.CharField(max_length=40)
-    usuario= models.ForeignKey(usuario, on_delete=models.CASCADE)
-    especialista = models.ForeignKey(especialista, on_delete=models.CASCADE)
-    prioridad = models.ForeignKey(prioridad, on_delete=models.CASCADE)
-    status = models.ForeignKey(status, on_delete=models.CASCADE)
-    coordenandas = models.CharField(max_length=40)
-    evidencia= models.ImageField()
-    descripcion = models.BigIntegerField()
-    problemas= models.ForeignKey(problemas, on_delete=models.CASCADE)
-    fecha_creacion= models.DateTimeField(verbose_name='Fecha creacion' )
+    folio = models.CharField(max_length=20)
+    titulo = models.CharField(max_length=5)
+    ticket_usariocreador = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    ticket_especialistaasignado = models.ForeignKey(especialista, on_delete=models.CASCADE)
+    ticket_tipoprioridad = models.ForeignKey(prioridad, on_delete=models.CASCADE)
+    ticket_tipostatus = models.ForeignKey(status_ticket, on_delete=models.CASCADE)
+    coordenadas = models.CharField(max_length=30)
+    evidencias= models.ImageField()
+    descripcion = models.CharField(max_length=100)
+    ticket_proyecto = models.ForeignKey(proyecto, on_delete=models.CASCADE)
+    ticket_areaorigen= models.ForeignKey(area, on_delete=models.CASCADE)
+    ticket_areadestino = models.ForeignKey(area_usuario, on_delete=models.CASCADE)
+    comentario = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField(verbose_name='Fecha creacion')
     fecha_atendido = models.DateTimeField(verbose_name='Fecha atendido')
     fecha_asignado = models.DateTimeField(verbose_name='Fecha asignado')
     fecha_proceso = models.DateTimeField(verbose_name='Fecha proceso')
@@ -73,31 +98,17 @@ class ticket (models.Model):
     fecha_resuelto = models.DateTimeField(verbose_name='Fecha resuelto')
     fecha_validado = models.DateTimeField(verbose_name='Fecha validado')
     fecha_cancelado = models.DateTimeField(verbose_name='Fecha cancelado')
-    comentario = models.BigIntegerField()
-
-class comentario (models.Model):
-
-    descripcion_ticket = models.CharField(max_length=40)
-    id_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
-    id_ticket= models.ForeignKey(ticket, on_delete=models.CASCADE)
-
+    ticket_superior = models.BigIntegerField()
+class comentario(models.Model):
+    descripcion_ticket = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField()
+    fecha_culminacion = models.DateTimeField()
+    comentario_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    comentario_ticket = models.ForeignKey(ticket, on_delete=models.CASCADE)
 class historial_ticket (models.Model):
 
-    id_ticket = models.ForeignKey(ticket, on_delete=models.CASCADE)
-    id_usuario = models.ForeignKey(usuario, on_delete=models.CASCADE)
+    ht_ticket = models.ForeignKey(ticket, on_delete=models.CASCADE)
+    ht_usuario = models.ForeignKey(usuario, on_delete = models.CASCADE)
     fecha_modificacion = models.DateTimeField(verbose_name='Fecha modificacion')
-    id_status_anterior= models.ForeignKey(status, on_delete=models.CASCADE)
-    id_especialidad_anterior = models.ForeignKey(especialidad, on_delete=models.CASCADE)
-
-
-class problemas (models.Model):
-
-    tipo_problemas = models.CharField(max_length=40)
-    fecha_creacion = models.DateTimeField(verbose_name='Fecha creacion' )
-    fecha_culminacion = models.DateTimeField(verbose_name='Fecha culminacion' )
-
-class ticket_relacionales  (models.Model):
-
-    id_ticket = models.ForeignKey(ticket, on_delete=models.CASCADE)
-    id_ticketn = models.ForeignKey(ticket, on_delete=models.CASCADE)
-    fecha_creacion = models.DateTimeField(verbose_name='Fecha creacion')
+    status_anterior = models.ForeignKey(status_ticket, on_delete=models.CASCADE)
+    especialista_anterior = models.ForeignKey(especialista, on_delete=models.CASCADE)
