3daa642951b098fd2ba858802ea09f90257b6f5f
Enoc FG <olaenoc54@gmail.com>
2022-10-10 16:24:26 -0500

    Choices

--- Helpdesk/settings.py
+++ Helpdesk/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Helpdesk.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'HelpdeskDB',
+        'NAME': 'DB_Helpdesk',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
---
+++ HelpdeskApp/choices.py
@@ -0,0 +1,30 @@
+status_tickets=(
+    ('1','Creado'),
+    ('2','Atendido'),
+    ('3','Asignado'),
+    ('4','Proceso'),
+    ('5','Espera'),
+    ('6','Resuelto'),
+    ('7','Validado'),
+    ('8','Cancelado'),
+    ('9','Reasignado')
+)
+
+status_entidades=(
+    ('1','Activo'),
+    ('2','Inactivo')
+)
+
+prioridades=(
+    ('1','Urgente'),
+    ('2','Alta'),
+    ('3','Media'),
+    ('4','Baja')
+)
+
+roles=(
+    ('SA', 'Super Administrador'),
+    ('GP', 'Gestor proyecto'),
+    ('ES', 'Especialista'),
+    ('UF', 'Operador')
+)
