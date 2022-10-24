7ade299d4f5bd3e45f75aaf1a09e800a4093d12f
Enoc Figueroa <olaenoc54@gmail.com>
2022-10-24 11:30:46 -0500

    Actualización de vistas para acceso-general

--- HelpdeskApp/views.py
+++ HelpdeskApp/views.py
@@ -12,77 +12,77 @@ from django.contrib.auth.decorators import login_required
 class EstatusViewSet(viewsets.ModelViewSet):
     queryset = status_e.objects.all()
     serializer_class = EstatusSerializer
-    permission_classes_by_action = {
-        'create': (permissions.IsAdminUser,),
-        'list': (permissions.IsAdminUser,),
-        'retrieve': (permissions.IsAdminUser,),
-        'update': (permissions.IsAdminUser,),
-        'destroy': (permissions.IsAdminUser,),
-        'search': (permissions.IsAdminUser,)  # <--- Option 1
-    }
+    # permission_classes_by_action = {
+    #     'create': (permissions.IsAdminUser,),
+    #     'list': (permissions.IsAdminUser,),
+    #     'retrieve': (permissions.IsAdminUser,),
+    #     'update': (permissions.IsAdminUser,),
+    #     'destroy': (permissions.IsAdminUser,),
+    #     'search': (permissions.IsAdminUser,)  # <--- Option 1
+    # }
 
 
 class areaViewSet(viewsets.ModelViewSet):
     queryset = area.objects.all()
     serializer_class = areaSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class proyectoViewSet(viewsets.ModelViewSet):
     queryset = proyecto.objects.all()
     serializer_class = proyectoSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class rolViewSet(viewsets.ModelViewSet):
     queryset = rol.objects.all()
     serializer_class = rolSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class espViewSet(viewsets.ModelViewSet):
     queryset = especialidad.objects.all()
     serializer_class = espSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class prioViewSet(viewsets.ModelViewSet):
     queryset = prioridad.objects.all()
     serializer_class = prioSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class usuarioViewSet(viewsets.ModelViewSet):
     queryset = usuario.objects.all()
     serializer_class = usuarioSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class especViewSet(viewsets.ModelViewSet):
     queryset = especialista.objects.all()
     serializer_class = especSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class ticketViewSet(viewsets.ModelViewSet):
     queryset = ticket.objects.all()
     serializer_class = ticketSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class EstatusTicketViewSet(viewsets.ModelViewSet):
     queryset = status_ticket.objects.all()
     serializer_class = EstatusTicketSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class ComentarioViewSet(viewsets.ModelViewSet):
     queryset = comentario.objects.all()
     serializer_class = ComentarioSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
 
 
 class HistorialViewSet(viewsets.ModelViewSet):
     queryset = historial_ticket.objects.all()
     serializer_class = HistorialTicketSerializer
-    permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAdminUser | IsAuthenticatedOrReadOnly]
