4049b6cdc7490acddbb10525e8043e428f9203e2
Enoc Figueroa <olaenoc54@gmail.com>
2022-11-03 11:01:31 -0600

    cambios en la base de datos

--- HelpdeskApp/models.py
+++ HelpdeskApp/models.py
@@ -1,40 +1,68 @@
 from django.db import models
 from django.utils import timezone
 
+from .choices import status_entidades
 from .choices import roles
 from .choices import prioridades
 from .choices import status_tickets
 
 
-# Clase status de las entidades correpondenes a proyecto, area, usuario, especialista
+# Clase Status entidad
 class status_e(models.Model):
-    tipo_estatus = models.CharField(max_length=100)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_culminacion = models.DateTimeField()
 
+    tipo_estatus = models.CharField(
+        max_length=50, choices=status_entidades, default='Inactivo')
+    #AUDITORIA
+    fecha_creacion = models.DateTimeField(
+        auto_now_add=True, verbose_name='Fecha incial')
+    fecha_actualizacion = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.tipo_estatus
 
 # Clase Proyecto
+
+
 class proyecto(models.Model):
-    nombre_proyecto = models.CharField(max_length=50)
-    codigo_proyecto = models.CharField(max_length=5)
-    fecha_inicial = models.DateTimeField(verbose_name='Fecha incial')
-    fecha_final = models.DateTimeField(verbose_name='Fecha final')
-    descripcion = models.TextField(null=True)
+
+    nombre_proyecto = models.CharField(max_length=150)
+    codigo_proyecto = models.CharField(max_length=10, unique=True)
+
+    fecha_inicial = models.DateTimeField(
+        null=True, verbose_name='Fecha incial')
+    fecha_final = models.DateTimeField(
+        null=True, verbose_name='Fecha final del proyecto')
+
+    descripcion = models.TextField(null=True, blank=True)
+
+    status_entidad = models.ForeignKey(status_e,
+                                       null=True, blank=True,
+                                       on_delete=models.CASCADE)
+
+    # AUDITORIA
     fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_culminacion = models.DateTimeField(auto_now=True)
-    fecha_actualizacion = models.DateTimeField(auto_now=True)
-    status_entidad = models.ForeignKey(status_e, null=True, blank=True, on_delete=models.CASCADE)
+    fecha_actualizacion = models.DateTimeField(null=True)
 
 
-# Clase Area para la tabla Proyecto
+    def __str__(self):
+        return self.nombre_proyecto
+
+
+# Clase Area
 class area(models.Model):
-    nombre_area = models.CharField(max_length=10)
-    codigo_area = models.CharField(max_length=5)
-    descripcion = models.TextField(null=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    nombre_area = models.CharField(max_length=150)
+    codigo_area = models.CharField(max_length=10, unique=True)
+    descripcion = models.TextField(blank=True)
     area_proyecto = models.ManyToManyField(proyecto)
-    status_entidad = models.ForeignKey(status_e, null=True, blank=True, on_delete=models.CASCADE)
+    status_entidad = models.ForeignKey(status_e,
+                                       null=True, blank=True,
+                                       on_delete=models.CASCADE)
+    # AUDITORIA
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.nombre_area
 
 
 # Clase Rol para la tabla Usuario
